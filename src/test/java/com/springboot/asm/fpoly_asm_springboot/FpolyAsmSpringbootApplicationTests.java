01b6750549adbcdad6a380485b6bb688417b1e55
haithach <haithach.01202@gmail.com>
2025-01-11 21:01:05 +0700

    first commit

---
+++ src/test/java/com/springboot/asm/fpoly_asm_springboot/FpolyAsmSpringbootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springboot.asm.fpoly_asm_springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FpolyAsmSpringbootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
