f329ea0b5aaf5f19180d05a81257a4ec6ebff622
Linh Pete <peterdamlinh1215@gmail.com>
2025-02-07 07:43:59 +0700

    update cart interface

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/controller/CartInterface.java
+++ src/main/java/com/springboot/asm/fpoly_asm_springboot/controller/CartInterface.java
@@ -2,8 +2,11 @@ package com.springboot.asm.fpoly_asm_springboot.controller;
 
 /**
  * **/
-public interface CartInterface<CartProdType> {
-    String getAllItems();
+public interface CartInterface<Cart, CartProdType> {
+    String getCartItems();
+    Cart retrieveLocalCart();
+    Cart retrieveRemoteCart();
+
     String addItem(CartProdType newProd);
     String removeItem(CartProdType removedProd);
     /**Change amount or design, etc. of the product in cart**/
@@ -15,4 +18,6 @@ public interface CartInterface<CartProdType> {
      * Save cart state to client local storage or sth when logout without paying
      * **/
     String saveCart();
+    String saveToLocalCart();
+    String saveToRemoteCart();
 }
