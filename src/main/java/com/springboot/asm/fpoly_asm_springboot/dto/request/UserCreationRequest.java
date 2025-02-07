307511c662bd3fc1af53edf8a7da4cc3b5dd83bf
giangnd99 <nguyendanggiang99@gmail.com>
2025-02-07 07:55:06 +0700

    remove Webconfig.class

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/dto/request/UserCreationRequest.java
+++ src/main/java/com/springboot/asm/fpoly_asm_springboot/dto/request/UserCreationRequest.java
@@ -1,5 +1,6 @@
 package com.springboot.asm.fpoly_asm_springboot.dto.request;
 
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Getter;
