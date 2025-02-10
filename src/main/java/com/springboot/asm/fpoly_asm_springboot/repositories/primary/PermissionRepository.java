30f7f87814bd5d2e615f0bfc03c61fd03a86c216
giangnd99 <nguyendanggiang99@gmail.com>
2025-02-10 08:17:49 +0700

    remove role, permission

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/primary/PermissionRepository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.asm.fpoly_asm_springboot.repositories.primary;
-
-import com.springboot.asm.fpoly_asm_springboot.entity.Permission;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface PermissionRepository extends JpaRepository<Permission, String> {
-}
--- src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/secondary/Role2Repository.java
+++
@@ -1,9 +0,0 @@
-package com.springboot.asm.fpoly_asm_springboot.repositories.secondary;
-
-import com.springboot.asm.fpoly_asm_springboot.entity.Role;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface Role2Repository extends JpaRepository<Role, String> {
-}
