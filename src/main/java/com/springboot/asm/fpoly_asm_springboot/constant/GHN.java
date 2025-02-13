d3982bbb59fdb1a12268ee90ae56db0331914eba
giangnd99 <nguyendanggiang99@gmail.com>
2025-02-13 15:32:34 +0700

    update GHN test api

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/constant/GHN.java
+++ src/main/java/com/springboot/asm/fpoly_asm_springboot/constant/GHN.java
@@ -4,11 +4,11 @@ public class GHN {
 
     public final static String TOKEN_API = "b253a5d3-e90b-11ef-aa00-0e2a2e30c300";
     public final static int SHOP_ID = 5630962;
-    public final static String API_URL_PROVINCE = "https://online-gateway.ghn.vn/shiip/public-api/master-data/province";
-    public final static String API_URL_DISTRICT = "https://online-gateway.ghn.vn/shiip/public-api/master-data/district";
-    public final static String API_URL_WARD = "https://online-gateway.ghn.vn/shiip/public-api/master-data/ward";
-    public final static String API_URL_SHIPPING_METHOD = "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services";
-    public final static String API_URL_SHIPPING_ORDER = "https://online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee";
+    public final static String API_URL_PROVINCE = "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/province";
+    public final static String API_URL_DISTRICT = "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/district";
+    public final static String API_URL_WARD = "https://dev-online-gateway.ghn.vn/shiip/public-api/master-data/ward";
+    public final static String API_URL_SHIPPING_METHOD = "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/available-services";
+    public final static String API_URL_SHIPPING_ORDER = "https://dev-online-gateway.ghn.vn/shiip/public-api/v2/shipping-order/fee\n";
 
     private GHN() {
     }
