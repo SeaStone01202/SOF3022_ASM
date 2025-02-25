84cf4f8cf7f972ef86ae98eb34d13f1f1637a38a
giangnd99 <nguyendanggiang99@gmail.com>
2025-02-25 18:04:02 +0700

    update

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/controller/DashboardController.java
+++ src/main/java/com/springboot/asm/fpoly_asm_springboot/controller/DashboardController.java
@@ -15,7 +15,7 @@ public class DashboardController {
     public List<Map<String, Object>> getDashboardData() {
         return List.of(
                 Map.of("title", "Tổng Đơn Hàng", "value", 500, "description", "Số đơn hàng đã xử lý.", "bgColor", "bg-dark"),
-                Map.of("title", "Doanh Thu", "value", "$50,000", "description", "Tổng doanh thu từ bán hàng.", "bgColor", "bg-success"),
+                Map.of("title", "Doanh Thu", "value", 50000, "description", "Tổng doanh thu từ bán hàng.", "bgColor", "bg-success"),
                 Map.of("title", "Số Khách Hàng", "value", 5000, "description", "Số khách hàng đã mua hàng.", "bgColor", "bg-info"),
                 Map.of("title", "Số Sản Phẩm", "value", 200, "description", "Tổng số sản phẩm đang bán.", "bgColor", "bg-warning")
         );
