97cedf92941993adba20821b5331ecd2c25ec63a
Hùng Phan <112998342+lamhungphan@users.noreply.github.com>
2025-02-25 19:44:30 +0700

    dashboard controller

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/primary/UserRepository.java
+++ src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/primary/UserRepository.java
@@ -4,8 +4,11 @@ import com.springboot.asm.fpoly_asm_springboot.entity.User;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Map;
 import java.util.Optional;
 
 @Repository
@@ -16,4 +19,11 @@ public interface UserRepository extends JpaRepository<User, Integer> {
     Optional<User> findByEmail(String email);
 
     Page<User> findAll(Pageable pageable);
+
+    @Query("SELECT new map(u.fullName as name, SUM(o.total) as total_spent) " +
+            "FROM ProductOrder o " +
+            "JOIN o.user u " +
+            "GROUP BY u.fullName " +
+            "ORDER BY SUM(o.total) DESC")
+    List<Map<String, Object>> getTopVipCustomers();
 }
