aee36bf41d403e62b686338504f3544c2d41cc9c
giangnd99 <nguyendanggiang99@gmail.com>
2025-02-26 08:37:31 +0700

    u

--- src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/primary/UserRepository.java
+++ src/main/java/com/springboot/asm/fpoly_asm_springboot/repositories/primary/UserRepository.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
+
 public interface UserRepository extends JpaRepository<User, Integer> {
 
     boolean existsByEmail(String email);
